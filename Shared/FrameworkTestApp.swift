558d7f8aedaac99dd3c15531d99b3be5b86bb610
Marco Gomiero <marco.gomiero.93@gmail.com>
2021-01-31 21:39:06 +0100

    Initial Commit

---
+++ Shared/FrameworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkTestApp.swift
+//  Shared
+//
+//  Created by Marco Gomiero on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
